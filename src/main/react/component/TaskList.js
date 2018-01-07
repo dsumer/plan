176b27067947655fa622e7f9378ca66e000551ea
Dominik Sumer <dominik.sumer@catalysts.cc>
2018-01-07 22:12:30 +0100

    added basic design

---
+++ src/main/react/component/TaskList.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Card, Label, Icon, Button } from 'semantic-ui-react';
+
+const TaskItem = ({color, name, duration, until}) => {
+    return (
+        <Card fluid color={color}>
+            <Card.Content>
+                <div className="button-wrapper">
+                    <Button animated>
+                        <Button.Content visible>
+                            <Icon name='write' />
+                        </Button.Content>
+                        <Button.Content hidden>Edit</Button.Content>
+                    </Button>
+                    <Button animated color="red">
+                        <Button.Content visible>
+                            <Icon name='delete' />
+                        </Button.Content>
+                        <Button.Content hidden>Delete</Button.Content>
+                    </Button>
+                </div>
+
+                <Card.Header>{name}</Card.Header>
+                <Card.Meta>{duration}</Card.Meta>
+                <Card.Description><Label color={color}>{until}</Label></Card.Description>
+            </Card.Content>
+
+            <style jsx>{`
+                .button-wrapper {
+                    position: absolute;
+                    right: 10px;
+                    top: 12px;
+                }
+                :global(.card .button) {
+                    display: block;
+                    margin-bottom: 5px;
+                }
+            `}</style>
+        </Card>
+    );
+};
+
+export default class TaskList extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h3 className="header">{this.props.header}</h3>
+
+                {
+                    this.props.items.map((item) => {
+                        return (
+                            <TaskItem key={item.id} color={this.props.color} {...item}/>
+                        );
+                    })
+                }
+
+                <style jsx>{`
+                    .header {
+                        margin-top: 50px;
+                    }
+                `}</style>
+            </React.Fragment>
+        );
+    }
+}
--- src/main/react/controller/index.js
+++ src/main/react/controller/index.js
@@ -1,12 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button, Icon } from 'semantic-ui-react';
+import TaskList from 'component/TaskList';
 
-const App = () => {
-    return (
-        <div>
-            <h2>Hiho React!</h2>
-        </div>
-    );
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="ui middle aligned center aligned grid">
+                    <div className="column">
+                        <h2>Your Tasks</h2>
+                        <Button animated color="green">
+                            <Button.Content visible>
+                                <Icon name='add' />
+                            </Button.Content>
+                            <Button.Content hidden>Add</Button.Content>
+                        </Button>
+
+                        <TaskList
+                            header="Overdue"
+                            color="red"
+                            items={[
+                                {
+                                    name: 'Code',
+                                    duration: 'Every 2 weeks',
+                                    until: 'Since 3 days'
+                                }
+                            ]}
+                        />
+
+                        <TaskList
+                            header="Today"
+                            color="green"
+                            items={[
+                                {
+                                    name: 'Code',
+                                    duration: 'Every 4 weeks',
+                                    until: 'Do it! :)'
+                                },
+                                {
+                                    name: 'Code',
+                                    duration: 'Every 4 weeks',
+                                    until: 'Do it! :)'
+                                }
+                            ]}
+                        />
+
+                        <TaskList
+                            header="Future"
+                            color="blue"
+                            items={[
+                                {
+                                    name: 'Code',
+                                    duration: 'Every 4 weeks',
+                                    until: 'In 7 days'
+                                }
+                            ]}
+                        />
+                    </div>
+                </div>
+
+                <style jsx>{`
+                  .grid {
+                    padding: 10px;
+                  }
+
+                  .column {
+                    max-width: 700px;
+                  }
+                `}</style>
+            </div>
+        );
+    }
 };
 
 ReactDOM.render(<App/>, document.getElementById('app'));
